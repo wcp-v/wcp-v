38b6e8208de4d03a500c2240705408dd49c00505
wcp-v <ichimimo00@gmail.com>
2021-06-17 07:46:18 +0000

    [Create]Address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_074008) do
+ActiveRecord::Schema.define(version: 2021_06_17_074524) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "address"
+    t.string "postcode"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
