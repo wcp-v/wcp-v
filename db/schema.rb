72ba976fbd6c1e7d5edd411cf73f3c7a878cd5ce
momose1031 <macbookair2020momose@icloud.com>
2021-06-17 07:41:23 +0000

    [create]order-item-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_072408) do
+ActiveRecord::Schema.define(version: 2021_06_17_074008) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2021_06_17_072408) do
     t.text "introduction"
     t.integer "price"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "count"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
