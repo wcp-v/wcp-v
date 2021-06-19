fa129ca69d3c2435dafd3fb835937421a8daa33d
Fumiya87 <ichimimo00@gmail.com>
2021-06-19 05:45:49 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :telephone_number, :email, :encrypted_password])
   end
   
  def after_sign_in_path_for(resource)
-   
+   git
   if resource_name == :admin
      admins_orders_path
   elsif resource_name == :customer
