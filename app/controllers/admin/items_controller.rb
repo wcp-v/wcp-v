7c08c349b331e8344e2782ae4b3ca452a5b5b921
momose1031 <macbookair2020momose@icloud.com>
2021-06-20 06:48:09 +0000

    test

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,12 @@
 class Admin::ItemsController < ApplicationController
-  # before_action :set_item, only: [:show, :edit, :update] 
+  # before_action :set_item, only: [:show, :edit, :update]
   # before_action :set_genres, only: [:new, :edit, :index, :create, :update]
   before_action :authenticate_admin!  #ログイン済のユーザのみアクセスを許可
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
@@ -15,20 +15,20 @@ class Admin::ItemsController < ApplicationController
 
   def index
   end
-  
+
   def show
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :image, :introduction, :price, :genre_id, :is_active)
   end
-  
+
 end
