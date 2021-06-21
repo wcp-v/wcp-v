c6263210942f5b183ecfa623638a6ad2d68f636b
Fumiya87 <ichimimo00@gmail.com>
2021-06-21 02:41:26 +0000

    order2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,20 +12,20 @@ class Admin::OrdersController < ApplicationController
       @order = Order.find(params[:id])
     order_items = @order.order_items
     @order.update(order_params)
-    if @order.order_status === "入金確認"
+    if @order.status === "入金確認"
       order_items.each do |order_item|
         order_item.update(order_item_params)
       end
     end
     @orders = Order.page(params[:page]).per(10)
-    redirect_to admin_orders_path
+    redirect_to admin_root_path
   end      
   
   private
 
-    def order_item_params
-      params.permit(:maiking_status).merge(maiking_status: 1)
-    end
+    # def order_item_params
+    #   params.permit(:maiking_status).merge(maiking_status: 1)
+    # end
 
     def order_params
       params.require(:order).permit(:order_status).merge(order_status: params[:order][:order_status].to_i)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'homes/top'
-  end
+  
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
