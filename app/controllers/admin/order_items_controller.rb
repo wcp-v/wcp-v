ab191615189b59288a3ef6eb35334b5a4b81d7a2
momose1031 <macbookair2020momose@icloud.com>
2021-06-25 11:02:59 +0000

    test

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,20 +1,16 @@
 class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
-
+  
   def update
     @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
     @order = @order_item.order
-    if @order_item.update(order_item_params)
-      redirect_to admin_order_path(@order_item)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    redirect_to admin_order_path(@order)
   end
-
+  
   private
-
+  
   def order_item_params
     params.require(:order_item).permit(:status)
   end
-
 end
